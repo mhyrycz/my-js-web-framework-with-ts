effac12623c7d260c4dbaaab815ebc5aa9c72426
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-08-24 16:45:40 +0200

    Event click added to button element.

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,17 +1,28 @@
 export class UserForm {
 	constructor(public parent: Element) {}
 
+	onButtonClick(): void {
+		console.log('dupa');
+	}
+
 	template(): string {
 		return `
             <div>
                 <h1> User Form</h1>
+                <button>Click me</button>
             </div>
         `;
+	}
+
+	bindEvents(templateElement: HTMLTemplateElement): void {
+		const button = templateElement.content.querySelectorAll("button")[0]
+		button.addEventListener('click',this.onButtonClick);
 	}
 
 	render(): void {
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
+		this.bindEvents(templateElement);
 		this.parent.append(templateElement.content);
 	}
 }
