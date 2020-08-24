e9d8e5a7358fb6897e8b97d3831b721cf6c146bf
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-08-24 19:04:50 +0200

    Rerender DOM on change event implemented.

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,30 +1,34 @@
-import {User} from '../models/User'
+import { User } from '../models/User';
 
 export class UserForm {
-	constructor(public parent: Element, public model: User) {}
+	constructor(public parent: Element, public model: User) {
+		this.model.on('change', () => {
+			this.render();
+		});
+	}
 
 	eventsMap(): { [key: string]: () => void } {
 		return {
-            //'click:.set-age': this.onSetAgeClick.bind(this)
-            'click:.set-age': this.onSetAgeClick
+			//'click:.set-age': this.onSetAgeClick.bind(this)
+			'click:.set-age': this.onSetAgeClick
 		};
-    }
-    
-    // this would be undefined if you invoke this function inside for loop, 
-    //that's why we use function declaration here instead of function expression
-    onSetAgeClick = (): void => {
-        this.model.setRandomAge()
-    }
+	}
+
+	// 'this' would be undefined if you invoke this function inside for loop,
+	//that's why we use function declaration here instead of function expression
+	onSetAgeClick = (): void => {
+		this.model.setRandomAge();
+	};
 
 	template(): string {
 		return `
             <div>
                 <h1> User Form</h1>
-                <div>${this.model.get("name")}</div>
-                <div>${this.model.get("age")}</div>
+                <div>${this.model.get('name')}</div>
+                <div>${this.model.get('age')}</div>
                 <button class='set-age'>set random age</button>
             </div>
-        `; 
+        `;
 	}
 
 	bindEvents(templateElement: DocumentFragment): void {
@@ -39,6 +43,7 @@ export class UserForm {
 	}
 
 	render(): void {
+		this.parent.innerHTML = '';
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
 		this.bindEvents(templateElement.content);
