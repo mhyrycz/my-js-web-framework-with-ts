c65aeeeebdc10cb129162402d7db42b07a71b65d
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-08-24 14:57:52 +0200

    Basic view class added.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
-import { UserProps, User } from './models/User';
-import { Collection } from './models/Collection';
+import { UserForm } from './views/UserForm';
 
-// const maciek = User.buildUser({ name: 'maciek', age: 28 });
+const userForm = new UserForm(document.getElementById('root'));
 
-// maciek.save();
-
-const dataCollection = User.buildUserCollection()
-
-dataCollection.on('collection fetched', () => {
-	console.log(dataCollection.getAll());
-});
-
-dataCollection.fetch();
-
-// new instance of User - my test approach
-// const testUser = new User(
-//    new Eventing(),
-//     new Sync("http://localhost:1235/users"),
-//     new Attributes<UserProps>({ name: "artur", age: 30})
-// )
-// testUser.save()
-
-
-let newUser = User.buildUser({ name: "artur", age: 30})
-
-console.log(newUser.get("age"))
+userForm.render();
